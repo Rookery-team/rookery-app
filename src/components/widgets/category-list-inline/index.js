4b010aed86f35230eac39c942defa84af42215e7
Ulysse ARNAUD <mexanga@gmail.com>
2021-06-18 21:04:25 +0200

    Correction styles

--- src/components/widgets/category-list-inline/index.js
+++ src/components/widgets/category-list-inline/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-// import styles from './index.module.scss';
+import './index.module.scss';
 import {Link} from "react-router-dom";
 import CategoryListInlineItem from "./list-item";
 
